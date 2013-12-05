ef2ee2df859f85035612720a1b7df400b29e98c6
Emmanuel.Gutierrez <emmanuel.gutierrez@cetys.edu.mx>
2013-12-04 19:35:01 -0800

    final

---
+++ Bitmap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Byte[] theBytes= new Byte[10];
+            r.NextBytes(theBytes);
+
+            Console.WriteLine("Los valores a buscar son :");
+            foreach(Byte b in theBytes)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine("Que valor deseas buscar?");
+            Byte read = Byte.Parse(Console.ReadLine());
+            int temp = 1;
+            foreach(var b in theBytes)
+            {
+                if (b == read)
+                    Console.WriteLine("Se encontro en el espacio {0}", temp);
+                temp++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
